a8f480b77f9fd55434a4f9a14aaa1eac1522b696
Ryan18 <164772732+yourapril01@users.noreply.github.com>
2026-07-20 10:05:40 +0700

    r9s: Add Raw Camera Support
    Hex Patching libexynoscamera3.so

--- objectives/r9s/vendor-patches/patches.sh
+++ objectives/r9s/vendor-patches/patches.sh
@@ -3,3 +3,12 @@
 # Latest unlockeable BL is based on binary I (one ui 7) so, we can use OneUI 8 vendor with vendor/tee folder from OneUI 7 (binary I)
 # Requires one ui 7 libsec-ril.so to have working ril
 REMOVE "vendor" "tee"
+
+# Enable RAW Support
+# Before: [cbz r0, #0x15612a]
+# After: [nop]
+HEX_PATCH "$WORKSPACE/vendor/lib/libexynoscamera3.so" "f0b12649" "00bf2649"
+
+# Before: [tbz w8, #0, 0x59d448]
+# After: [nop]
+HEX_PATCH "$WORKSPACE/vendor/lib64/libexynoscamera3.so" "88020036610d00f0" "1f2003d5610d00f0"
