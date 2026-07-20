674ef80e12c9ca072bc0bd4bbd06fa8573367d03
Ryan18 <164772732+yourapril01@users.noreply.github.com>
2026-07-20 19:17:04 +0700

    misc: Bomb useless patch

--- astro/misc/ssrm/SamsungDeviceHealthManagerService.apk/Remove-SSRM-Warning.sh
+++
@@ -1,2 +0,0 @@
-find . -type f -name "*.smali" -exec sed -i 's/siop_model/siop_model/g' {} +
-find . -type f -name "*.smali" -exec sed -i 's/dvfs_policy_exynos2100_xx/dvfs_policy_exynos2100_xx/g' {} +
